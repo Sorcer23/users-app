2f634adfee5b7b91e123fc27eac9cb5e94f0b957
Andrii Valentiuk <andriy.valenyuk@gmail.com>
2023-06-09 03:13:50 +0300

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Users app</div>;
+};
+
+export { App };
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+
+const config = ({ mode }) =>
+  defineConfig({
+    resolve: {
+      alias: {
+        src: resolve(__dirname, 'src'),
+      },
+    },
+    plugins: [react(), eslint()],
+    optimizeDeps: {
+      esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+          global: 'globalThis',
+        },
+      },
+    },
+    css: {
+      modules: {
+        localsConvention: 'camelCase',
+        generateScopedName: mode === 'production' ? '[hash:base64:5]' : '[name]_[local]_[hash:base64:5]',
+      },
+    },
+  });
+
+export default config;
